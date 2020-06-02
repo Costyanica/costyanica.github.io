eb2edaebdac789a72f64e156cc7e95f228d51fda
Kseniya Molkova <52568987+Costyanica@users.noreply.github.com>
2020-06-02 17:05:15 +0300

    test_task

---
+++ script.js
@@ -0,0 +1,15 @@
+// const buttons = document.querySelectorAll('.button');
+
+// const watchMore = (item) => {
+//     item.toElement.classList.add('active');
+// }
+
+
+// const hideAll = (item) => {
+//     item.toElement.classList.remove('active');
+// }
+
+
+// buttons.forEach((item) => item.addEventListener('mouseover', watchMore));
+
+// buttons.forEach((item) => item.addEventListener('mouseout', hideAll));
